4c9079777be7858429abe0b22b20ed3dfa027bc2
Deki Lhazom <12210048.gcit@rub.edu.bt>
2023-11-28 17:25:16 +0000

    Updated

--- app.js
+++ app.js
@@ -3,15 +3,15 @@ const path = require('path')
 const userRouter = require('./routes/userRoutes')
 const viewRouter = require('./routes/viewRoutes')
 const applicationRouter = require('./routes/applicationRoutes')
-const session = require('express-session');
+// const session = require('express-session');
 const app = express();
 
-app.use(session({
-    secret: '47c57b15e203fbe46898015c57f2d98ff64827347c9deef48a36f0200c15341f', // Replace with a secret key for session encryption
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } // Set to true if your application is served over HTTPS
-  }));
+// app.use(session({
+//     secret: '47c57b15e203fbe46898015c57f2d98ff64827347c9deef48a36f0200c15341f', // Replace with a secret key for session encryption
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: false } // Set to true if your application is served over HTTPS
+//   }));
 
 app.use(express.json())
 const cookieParser = require('cookie-parser')
